6f0e56555c9d1a2e82ef1bfe6989a38cd91f2ec7
lapointek <lapointek.dev@gmail.com>
2025-10-17 13:19:08 -0400

    feat: add logic for guessing secret phrase
    feat: add switch statement to display hangman

--- hangman/hangman.cpp
+++ hangman/hangman.cpp
@@ -1,25 +1,178 @@
 #include <iostream>
+#include <cstring>
+#include <cctype>
+
 using namespace std;
 
+const int IGNORE_CHARS = 256;
+
+const char *INPUT_ERROR_STRING = "Input error! Please try again.";
 void PlayGame();
-void WantToPlayAgain();
+int GetSecretPhrase(char secretPhrase[], int maxLength);
+char *MakeHiddenPhrase(const char *secretPhrase, int secretPhraseLength);
+bool WantToPlayAgain();
+void DrawBoard(int numberOfGuessesLeft, const char *optrHiddenPhrase);
 
-int main() {
+int main()
+{
+  do
+  {
+    PlayGame();
+  } while (WantToPlayAgain());
 
-  do {
-
-  } while (WantToPlayAgain())
-
-      return 0;
+  return 0;
 }
 
-void WantToPlayAgain() {}
+bool WantToPlayAgain() {}
 
-void PlayGame() {
+void PlayGame()
+{
   const int MAX_LENGTH_OF_SECRET_PHRASE = 256;
-  char secretPhrase[MAX_LENGTH_OF_SECRET_PHRASE];
   const int MAX_NUMBER_OF_GUESSES = 6;
 
-  char *hiddenPhrase = nullptr;
+  char secretPhrase[MAX_LENGTH_OF_SECRET_PHRASE];
+  char *optrHiddenPhrase = nullptr;
   int numberOfGuessesLeft = MAX_NUMBER_OF_GUESSES;
+  int secretPhraseLength = GetSecretPhrase(secretPhrase, MAX_LENGTH_OF_SECRET_PHRASE);
+
+  optrHiddenPhrase = MakeHiddenPhrase(secretPhrase, secretPhraseLength);
+}
+
+int GetSecretPhrase(char secretPhrase[], int maxLength)
+{
+  bool failure;
+  int length;
+  do
+  {
+    failure = false;
+    cout << "Please enter te secret phrase: ";
+    cin.get(secretPhrase, maxLength);
+
+    if (cin.fail())
+    {
+      cin.clear();
+      cin.ignore(IGNORE_CHARS, '\n');
+      cout << INPUT_ERROR_STRING << endl;
+      failure = true;
+    }
+    else
+    {
+      length = strlen(secretPhrase);
+
+      if (length == 0)
+      {
+        cout << "You must enter a word that's longer than 0 characters! Please try again." << endl;
+        failure = true;
+      }
+    }
+  } while (failure);
+
+  return length;
+}
+
+char *MakeHiddenPhrase(char *secretPhrase, int secretPhraseLength)
+{
+  char *hiddenPhrase = new char[secretPhraseLength + 1];
+
+  for (int i = 0; i < secretPhraseLength; i++)
+  {
+    if (secretPhrase[i] != ' ')
+    {
+      hiddenPhrase[i] = '-';
+    }
+    else
+    {
+      hiddenPhrase[i] = ' ';
+    }
+  }
+  hiddenPhrase[secretPhraseLength] = '\0';
+  return hiddenPhrase;
+}
+
+void DrawBoard(int numberOfGuessesLeft, const char *optrHiddenPhrase)
+{
+  switch (numberOfGuessesLeft)
+  {
+  case 0:
+  {
+    cout << " +---+" << endl;
+    cout << " |   |" << endl;
+    cout << " |   O" << endl;
+    cout << " |  /|\\" << endl;
+    cout << " |  / \\" << endl;
+    cout << "_+_" << endl
+         << endl;
+  }
+  break;
+  case 1:
+  {
+    cout << " +---+" << endl;
+    cout << " |   |" << endl;
+    cout << " |   O" << endl;
+    cout << " |  /|\\" << endl;
+    cout << " |  / " << endl;
+    cout << "_+_" << endl
+         << endl;
+  }
+  break;
+  case 2:
+  {
+    cout << " +---+" << endl;
+    cout << " |   |" << endl;
+    cout << " |   O" << endl;
+    cout << " |  /|\\" << endl;
+    cout << " |  " << endl;
+    cout << "_+_" << endl
+         << endl;
+  }
+  break;
+  case 3:
+  {
+    cout << " +---+" << endl;
+    cout << " |   |" << endl;
+    cout << " |   O" << endl;
+    cout << " |  /|" << endl;
+    cout << " |  " << endl;
+    cout << "_+_" << endl
+         << endl;
+  }
+  break;
+  case 4:
+  {
+    cout << " +---+" << endl;
+    cout << " |   |" << endl;
+    cout << " |   O" << endl;
+    cout << " |  /" << endl;
+    cout << " |  " << endl;
+    cout << "_+_" << endl
+         << endl;
+  }
+  break;
+  case 5:
+  {
+    cout << " +---+" << endl;
+    cout << " |   |" << endl;
+    cout << " |   O" << endl;
+    cout << " |  " << endl;
+    cout << " |  " << endl;
+    cout << "_+_" << endl
+         << endl;
+  }
+  break;
+  case 6:
+  {
+    cout << " +---+" << endl;
+    cout << " |   |" << endl;
+    cout << " |   " << endl;
+    cout << " |  " << endl;
+    cout << " |  " << endl;
+    cout << "_+_" << endl
+         << endl;
+  }
+  break;
+  default:
+    break;
+  }
+  cout << "Secret Phrase: " << optrHiddenPhrase << endl
+       << endl;
 }
